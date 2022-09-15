f08e4db92e0ab95a0c28d6d92c091f40e0a5caeb
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-09-15 06:23:17 +0530

    git push

---
+++ 1.Basic/0Why.java
@@ -0,0 +1,10 @@
+/*
+Why should we use java?
+
+=> it is platform independent. (platform means windows+intel, windows+amd, linux+amd). bcz java make its code into BYTECODE and then most machines have JVM which is responsible for running byte code in a machine
+=> simple compilation but complex running process than c/c++
+=> not have concepts like pointers, operator overloading that makes it simple
+=> unlike python you need to declare variable type which make program faster
+
+
+*/
