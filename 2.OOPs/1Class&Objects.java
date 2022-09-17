51732283456998e6aad38453865d2308a2e2c077
Rounak Mukherjee <80785919+Rounak00@users.noreply.github.com>
2022-09-17 16:31:42 +0530

    git push

--- 2.OOPs/1Class&Objects.java
+++ 2.OOPs/1Class&Objects.java
@@ -35,3 +35,104 @@ public class Gfg
     } 
 } 
 
+
+//..............................................AccessSpecifier............................................................
+Private protected public default.... here the problem is about default which is new in java. its same like public but its can help you to run obbject only of that package;
+
+//................................................This.....................................................................
+class Point
+{
+    int x, y;
+
+    Point(int x, int y)
+    {
+        this.x = y;
+        this.y = y;
+    }
+
+    void print()
+    {
+        System.out.println(x+" "+y);
+    }
+}
+public class Gfg
+{ 
+    public static void main(String[] args)throws IOException 
+    { 
+      
+      Point p1 = new Point(10, 20);
+      
+      Point p2 = new Point(5, 15);
+
+     
+
+    } 
+} 
+
+
+//.................................................Static Member.........................................................
+import java.math.*;
+import java.io.*;
+import java.util.*;
+
+class Player
+{
+    String name;
+    int id;
+
+    static int playerCount = 0;
+
+    Player(String name)
+    {
+        this.name = name;
+        id= ++playerCount;
+    }
+
+    void printDetails()
+    {
+        System.out.println(id+":"+name);
+    }
+}
+public class Gfg
+{ 
+    public static void main(String[] args)throws IOException 
+    { 
+      Player p1 = new Player("xyz");
+      Player p2 = new Player("abc");
+
+      p1.printDetails();
+      p2.printDetails();
+    } 
+} 
+
+
+
+//static methhods
+public class GFG {
+    // static variable
+    static int a = 40;
+  
+    // instance variable
+    int b = 50;
+  
+    void simpleDisplay()
+    {
+        System.out.println(a);
+        System.out.println(b);
+    }
+  
+    // Declaration of a static method.
+    static void staticDisplay()
+    { 
+      System.out.println(a); 
+    }
+  
+    // main method
+    public static void main(String[] args)
+    {
+        GFG obj = new GFG();
+        obj.simpleDisplay();
+  
+        // Calling static method.
+        staticDisplay();
+    }}
---
+++ 2.OOPs/3Final.java
@@ -0,0 +1,63 @@
+//Variable
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		
+		final int x;
+		
+		x = 100;
+		
+		System.out.println(x);
+	}
+}
+//Class Final variable
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class Test {
+    
+   static final int MAX = 100;
+    
+	public static void main (String[] args) {
+		
+		
+		
+		System.out.println(MAX);
+	}
+}
+//Instance Final variable
+
+import java.io.*;
+
+class Test {
+    
+    final int x = 100;
+    
+	public static void main (String[] args) {
+		
+		Test t =  new Test();
+		
+		System.out.println(t.x);
+	}
+}
+//Final refference variable
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+class Test {
+    
+    
+	public static void main (String[] args) {
+		
+		final StringBuffer sb = new StringBuffer("Geeks");
+		
+		sb.append("forGeeks");
+		
+		System.out.println(sb);
+	}
+}
